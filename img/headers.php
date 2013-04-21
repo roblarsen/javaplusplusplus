f8277d110a1326a88db1bd3a140ce7bed750d426
Rob Larsen <rob@htmlcssjavascript.com>
2013-04-21 18:32:14 -0400

    time to back this thing up
    IT'S BEEN A WHILE

--- img/headers.php
+++ img/headers.php
@@ -9,7 +9,7 @@ while ($file = readdir($dir)) {
 
     $files[] = $file;
 }
-header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + 31536000));
+header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + 36000));
 header('Content-type: application/json');
 echo json_encode($files);
 ?>
--- jppp.js
+++ jppp.js
@@ -20,23 +20,26 @@
 		      var $el = $(el);
           $el.removeAttr("height").css("max-width", $el.width()).attr("width", "100%")});
 		    });
-        $( document ).on('movestart', function(e) {
+     /*   $( "#wrap" ).on('movestart', function(e) {
           if ((e.distX > e.distY && e.distX < -e.distY) ||
               (e.distX < e.distY && e.distX > -e.distY)) {
             e.preventDefault();
           }
         });
-       $( document ).on("swipeleft",function(){
+       $( "#wrap" ).on("swipeleft",function(){
           if ($( "[rel=next]" ).length) {
             window.location.href = $( "[rel=next]" ).attr("href");  
           }
           
         });
-        $( document ).on("swiperight",function(){
+        $( "#wrap" ).on("swiperight",function(){
           if ($( "[rel=prev]" ).length) {
             window.location.href = $( "[rel=prev]" ).attr("href");
           }
         });
+               var $div = $("<div/>").text("hint: you can navigate through posts by swiping left and right").css({"position":"absolute", "top":"200px", "width" : "300px", "text-align" : "center", "padding" :"5px","background" : "#fff", "opacity" : "0"});
+    $("body").append($div);
+    $($div).animate({"opacity" : "1", "left":"40%"}, 5000, function(){ $($div).animate({"opacity" : "0", "left":"100%"}, 2000, function(){ }) });*/
       }
 	  }
   }())._init();
